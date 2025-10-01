e5a0e21dc3cedbbc9e36a6d88c5aa612107e6eca
avisualprod <avisualproducciones@gmail.com>
2025-10-01 17:12:00 -0300

    initial add

---
+++ 01-main.py
@@ -0,0 +1,35 @@
+print('Hola mundo')
+
+# var_string = "esto es un string"
+# print(type(var_string))
+
+# var_integer = 101
+# print(var_integer, "su tipo de dato es:", type(var_integer))
+
+# var_float = 3.141521
+# print(var_float, "su tipo de dato es:", type(var_float))
+
+# var_bool = True
+# print(var_bool, "Su tipo de dato es:", type(var_bool))
+
+Valor_1 = 3
+Valor_2 = 5.43
+print(Valor_1 + int(Valor_2))
+print(type(Valor_1), type(Valor_2))
+
+# 0 AND 0 --> 0   ----> and
+# 0 AND 1 --> 0
+# 1 AND 0 --> 0
+# 1 AND 1 --> 1
+
+# 0 OR 0 --> 0     ---->or
+# 0 OR 1 --> 1
+# 1 OR 0 --> 1
+# 1 OR 1 --> 1
+
+# 0 ORX 0 --> 0  ----> 
+# 0 ORX 1 --> 1
+# 1 ORX 0 --> 1
+# 1 ORX 1 --> 0
+
+print()
